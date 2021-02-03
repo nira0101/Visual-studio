8d8eee235b6000bbc854e525b26101c7f3de9ae7
nirajan <nira0101@stud.kea.dk>
2021-02-03 18:08:39 +0100

    added modal in js

--- js/script.js
+++ js/script.js
@@ -1,11 +1,12 @@
-fetch("./actors.json")
-.then(function(resp){
+let fetchData = fetch("./actors.json")
+.then(function(response){
   
-    return resp.json();
+    return response.json();
 })
 .then(function(data){
     
     nameReceived(data);
+    
 })
 
 function nameReceived(names){
@@ -13,10 +14,28 @@ function nameReceived(names){
 
 }
 function displayName(oneName){
- console.log(oneName)
+ 
  const temp = document.querySelector("#actorNames").content;
  const nameCopy = temp.cloneNode(true);
 
+ nameCopy.querySelector("h3").textContent = oneName.fullname;
+
+
+  nameCopy.querySelector("article.actors").addEventListener("click", (e) =>showDetails(oneName))
+
+ 
  const appendLocation = document.querySelector(".grids");
  appendLocation.appendChild(nameCopy);
 }
+
+ function showDetails(data){
+  console.log(data)
+   modal.querySelector(".modal-name").textContent = data.movie;
+   modal.classList.remove("hide");
+  
+} 
+
+const modal = document.querySelector(".modal-background");
+modal.addEventListener("click", () => {
+    modal.classList.add("hide");
+})
