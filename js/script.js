baf128278b67354b83e5a117b8a4d24c8c6ea689
nirajan <nira0101@stud.kea.dk>
2021-02-03 12:00:47 +0100

    added template and cloned it

--- js/script.js
+++ js/script.js
@@ -1,7 +1,22 @@
 fetch("./actors.json")
 .then(function(resp){
+  
     return resp.json();
 })
 .then(function(data){
-    console.log(data);
+    
+    nameReceived(data);
 })
+
+function nameReceived(names){
+    names.forEach(displayName);
+
+}
+function displayName(oneName){
+ console.log(oneName)
+ const temp = document.querySelector("#actorNames").content;
+ const nameCopy = temp.cloneNode(true);
+
+ const appendLocation = document.querySelector(".grids");
+ appendLocation.appendChild(nameCopy);
+}
